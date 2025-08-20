d9c5c5dbf6afb562a362e658366c1c7c0eab237b
moneos1 <moneos.ngba@gmail.com>
2025-08-20 16:57:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function connectWallet() {
+    document.getElementById("output").innerText = "✅ Wallet connecté (simulation)";
+}
+
+function stake() {
+    document.getElementById("output").innerText = "💰 Staking de 100 € effectué (simulation)";
+}
